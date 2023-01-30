2acc7f0e70cf93d145d6aa45be1dff628a66163e
2peagles <precious.d.bey@gmail.com>
2023-01-29 20:05:34 -0500

    form

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,12 +1,31 @@
 import React from 'react'
 import '../styles/contact.scss'
+
+
+function handleSubmit(event) {
+  event.preventDefault();
+
+  const form = event.target;
+  const name = form.elements.name.value;
+  const email = form.elements.email.value;
+  const phone = form.elements.phone.value;
+  const message = form.elements.message.value;
+  
+  console.log({ name, email, phone, message });
+
+  // Perform your desired actions here, such as sending the data to a server.
+}
+// This function listens for the form's submit event, prevents the default form submission, and extracts the values from the form elements. 
+// It then logs the extracted values to the console for demonstration purposes. You would replace the console.log statement with your desired 
+// actions, such as sending the data to a server using fetch or XMLHttpRequest.
 const Contact = () => {
   return (
     <div id='contact'>
       <article>
         <div>
           <h1> contact us</h1>
-          <p>Ready to take it to the next level? Let's talk about your project or idea and find out how we can help your busines grow.If you are looking for unique digital experiences that's relatable to your users, drop us a line.</p>
+          <p>Ready to take it to the next level? Let's talk about your project or idea and find out how we can help your busines grow.
+                  If you are looking for unique digital experiences that's relatable to your users, drop us a line.</p>
         </div>
         <form>
           <input placeholder='name' required type='name' />
@@ -17,7 +36,7 @@ const Contact = () => {
           <hr></hr>
           <textarea placeholder='message' required type='text' maxlength="100" />
           <hr></hr>
-          <button className='darkbtn' onSumbit={()=>alert(' We will get back to you shortly!')}> submit </button>
+          <button className='darkbtn' onSubmit={handleSubmit}> submit </button>
         </form>
       </article>
       <article className='locations'>
